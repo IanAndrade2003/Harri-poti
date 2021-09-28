776ddb27e5fd4f86411126d38f4251b844263001
IanAndrade2003 <ianandrade90@outlook.com>
2021-09-27 23:07:22 -0300

    thirdPhase

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,8 @@ export default function App() {
     const BoxZoeira = styled.div`
     display: flex;
     width: 100%
+    background: rgb(209,228,245);
+background: radial-gradient(circle, rgba(209,228,245,0.9864320728291317) 0%, rgba(164,204,241,1) 13%, rgba(110,178,240,1) 27%, rgba(80,163,238,1) 41%, rgba(61,149,235,1) 54%, rgba(40,140,240,1) 67%, rgba(12,108,212,1) 80%, rgba(3,77,162,1) 90%, rgba(1,45,96,1) 100%);
     `;
 
     const Paragraph = styled.p`
--- src/components/Main.js
+++ src/components/Main.js
@@ -11,13 +11,17 @@ export default function App() {
 
   const Box = styled.div`
     display: flex;
+    background: rgb(209,228,245);
+background: radial-gradient(circle, rgba(209,228,245,0.9864320728291317) 0%, rgba(164,204,241,1) 13%, rgba(110,178,240,1) 27%, rgba(80,163,238,1) 41%, rgba(61,149,235,1) 54%, rgba(40,140,240,1) 67%, rgba(12,108,212,1) 80%, rgba(3,77,162,1) 90%, rgba(1,45,96,1) 100%);
     flex-wrap: wrap;
   `;
 
   const BoxPerso = styled.div`
     margin-left: 60px;
     margin-bottom: 15px;
-    border: 4px solid #4ebdc1;
+    border: 4px solid #000000;
+    background: rgb(209,228,245);
+background: radial-gradient(circle, rgba(209,228,245,0.9864320728291317) 0%, rgba(164,204,241,1) 13%, rgba(110,178,240,1) 27%, rgba(80,163,238,1) 41%, rgba(61,149,235,1) 54%, rgba(40,140,240,1) 67%, rgba(12,108,212,1) 80%, rgba(3,77,162,1) 90%, rgba(1,45,96,1) 100%);
   `;
 
   const Paragraph = styled.p`
